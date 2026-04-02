be833b62d6c642d55f5fd7c0ce2033249a37d81a
alumnaslt <alumnaslt@gmail.com>
2026-04-02 20:04:40 +0300

    Add new alumni mentors and availability

--- mentors-fallback-data.js
+++ mentors-fallback-data.js
@@ -222,6 +222,258 @@ window.ALUMNAS_MENTOR_SEEDS = [
       "Viešųjų ryšių ir žurnalistikos skirtumų aptarimu",
       "Pirmo kurso studentiškos patirties pasidalijimu"
     ]
+  },
+  {
+    "id": "rolandas-vdu",
+    "name": "Rolandas",
+    "university": "VDU",
+    "studyProgram": "Viešasis administravimas",
+    "studyYear": "1 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/rolandas-vdu.jpg",
+    "facts": [
+      "Turi katiną vardu Batonas",
+      "Moka kalbėti paukščių kalba",
+      "Nepalieka ant seen ir visada padės kuo galės"
+    ],
+    "tags": [
+      "VDU",
+      "Viešasis administravimas",
+      "Studijų pasirinkimas"
+    ],
+    "bio": "Rolandas dalijasi viešojo administravimo studijų pradžios patirtimi ir padeda ramiau suprasti, ko tikėtis pirmame kurse.",
+    "helpWith": [
+      "Klausimais apie viešojo administravimo studijas VDU",
+      "Pirmo kurso studijų realybės aptarimu",
+      "Asmeninės patirties pasidalijimu prieš stojimą"
+    ]
+  },
+  {
+    "id": "gabija-lsmu",
+    "name": "Gabija",
+    "university": "LSMU",
+    "studyProgram": "Kineziterapija",
+    "studyYear": "2 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/gabija-sakalauskait.jpg",
+    "facts": [
+      "Derina kineziterapijos studijas, gydomojo masažo mokslus ir darbą",
+      "Per dvejus metus išmoko efektyviai planuoti savo laiką",
+      "Yra komunikabili, motyvuota, išklausanti ir supratinga"
+    ],
+    "tags": [
+      "LSMU",
+      "Kineziterapija",
+      "Laiko planavimas"
+    ],
+    "bio": "Gabija dalijasi kineziterapijos studijų kasdienybe, adaptacijos procesu ir tuo, kaip suderinti du mokslus su darbu.",
+    "helpWith": [
+      "Kineziterapijos studijų kasdienybės aptarimu",
+      "Klausimais apie adaptaciją universitete",
+      "Studijų, papildomų mokslų ir darbo derinimu"
+    ]
+  },
+  {
+    "id": "austeja-lsmu",
+    "name": "Austėja",
+    "university": "LSMU",
+    "studyProgram": "Odontologija",
+    "studyYear": "2 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/austeja-lsmuodontologija.jpg",
+    "facts": [
+      "Darbą susijusį su specialybe rado jau 1 kurse",
+      "Pusę metų savanoriavo ligoninėje ir pusę metų darželyje",
+      "Buvo mergaičių futbolo komandos kapitonė"
+    ],
+    "tags": [
+      "LSMU",
+      "Odontologija",
+      "Savanorystė"
+    ],
+    "bio": "Austėja atvirai pasakoja apie odontologijos studijų pradžią, pirmą darbą pagal specialybę ir aktyvią veiklą už universiteto ribų.",
+    "helpWith": [
+      "Klausimais apie odontologijos studijas LSMU",
+      "Pirmų kursų patirties ir galimybių aptarimu",
+      "Savanorystės ir darbo pagal specialybę temomis"
+    ]
+  },
+  {
+    "id": "rasa-lmta",
+    "name": "Rasa",
+    "university": "LMTA Klaipėdos fakultetas",
+    "studyProgram": "Šokis ir edukacija",
+    "studyYear": "1 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/rasa-sabeckait.jpg",
+    "facts": [
+      "Šoka daugiau valandų nei miega",
+      "Iššūkiai ją motyvuoja niekada nesustoti ir žengti į priekį",
+      "Jos gyvenimas sukasi apie kūrybą ir naujas idėjas"
+    ],
+    "tags": [
+      "LMTA",
+      "Šokis ir edukacija",
+      "Kūryba"
+    ],
+    "bio": "Rasa dalijasi šokio ir edukacijos studijų ritmu, kūrybine kasdienybe ir tuo, kaip atrodo aktyvus pirmo kurso gyvenimas.",
+    "helpWith": [
+      "Klausimais apie šokio ir edukacijos studijas",
+      "Kūrybinės krypties pasirinkimo aptarimu",
+      "Pirmo kurso patirties pasidalijimu"
+    ]
+  },
+  {
+    "id": "nojus-vdu",
+    "name": "Nojus",
+    "university": "VDU",
+    "studyProgram": "Marketingas",
+    "studyYear": "4 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/nojus-marketingasvdu.jpg",
+    "facts": [
+      "Mėgsta krepšinį ir laisvalaikiu žaidžia jį",
+      "Yra tikras muzikos melomanas ir Shazamas",
+      "Dažnai užmiega skaitydamas paskaitų medžiagą"
+    ],
+    "tags": [
+      "VDU",
+      "Marketingas",
+      "Komunikacija"
+    ],
+    "bio": "Nojus dalijasi marketingo studijų patirtimi ir padeda geriau suprasti, kaip atrodo ketvirto kurso kasdienybė VDU.",
+    "helpWith": [
+      "Klausimais apie marketingo studijas VDU",
+      "Studijų krypties pasirinkimo aptarimu",
+      "Studentiškos kasdienybės pasidalijimu"
+    ]
+  },
+  {
+    "id": "odeta-viko",
+    "name": "Odeta",
+    "university": "Vilniaus kolegija",
+    "studyProgram": "Ergoterapija",
+    "studyYear": "2 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/odeta-ergoterapija.jpg",
+    "facts": [
+      "Studijuoja ergoterapiją ir ją žavi ši dar daug kam nepažįstama, bet paklausi sritis",
+      "Yra empatiška ir geranoriška, todėl visada stengiasi palaikyti ir padėti",
+      "Muzika, šokiai, rankdarbiai, dizainas ir stilistika jai yra naujų idėjų šaltinis"
+    ],
+    "tags": [
+      "Vilniaus kolegija",
+      "Ergoterapija",
+      "Sveikatos mokslai"
+    ],
+    "bio": "Odeta dalijasi ergoterapijos studijų patirtimi ir padeda geriau suprasti šią dar mažiau pažįstamą, bet labai prasmingą kryptį.",
+    "helpWith": [
+      "Klausimais apie ergoterapijos studijas",
+      "Sveikatos mokslų krypties pasirinkimo aptarimu",
+      "Studijų kasdienybės ir galimybių pasidalijimu"
+    ]
+  },
+  {
+    "id": "leja-lsmu",
+    "name": "Leja",
+    "university": "LSMU",
+    "studyProgram": "Veterinarinė medicina",
+    "studyYear": "2 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/leja-ft.jpeg",
+    "facts": [
+      "Jos namuose visada gyvūnų globos centras - nuo antenuotų iki sparnuotų",
+      "Trumpai ir aiškiai: miškas yra geriau už miestą",
+      "Vaikystėje sakė, kad bus prezidentė, bet liko laisvo oro direktore"
+    ],
+    "tags": [
+      "LSMU",
+      "Veterinarinė medicina",
+      "Gyvūnai"
+    ],
+    "bio": "Leja dalijasi veterinarinės medicinos studijų realybe ir padeda suprasti, kaip atrodo kelias tiems, kurie myli gyvūnus.",
+    "helpWith": [
+      "Klausimais apie veterinarinės medicinos studijas LSMU",
+      "Gyvūnų priežiūros ir studijų krypties aptarimu",
+      "Asmeninės patirties pasidalijimu prieš stojimą"
+    ]
+  },
+  {
+    "id": "ieva-marija-ism",
+    "name": "Ieva Marija",
+    "university": "ISM",
+    "studyProgram": "Tarptautinis verslas ir komunikacija",
+    "studyYear": "3 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/ieva-marija-geceviciute.jpg",
+    "facts": [
+      "Renginių organizavimas yra jos secret talent",
+      "Nuo pirmų studijų dienų aktyviai dalyvauja universiteto renginiuose ir veiklose",
+      "Universitetui atstovavo ne vienoje studijų mugėje ir žino viską nuo A iki Z"
+    ],
+    "tags": [
+      "ISM",
+      "Tarptautinis verslas",
+      "Komunikacija"
+    ],
+    "bio": "Ieva Marija dalijasi tarptautinio verslo ir komunikacijos studijų patirtimi bei tuo, kaip atrodo aktyvus gyvenimas ISM bendruomenėje.",
+    "helpWith": [
+      "Klausimais apie tarptautinio verslo ir komunikacijos studijas",
+      "Renginių, universiteto veiklų ir studijų mugių aptarimu",
+      "ISM aplinkos ir studentiškos patirties pasidalijimu"
+    ]
+  },
+  {
+    "id": "viktorija-ism",
+    "name": "Viktorija",
+    "university": "ISM",
+    "studyProgram": "Tarptautinis verslas ir komunikacija",
+    "studyYear": "2 kursas",
+    "rating": 5,
+    "reviewCount": 0,
+    "duration": 45,
+    "price": 1299,
+    "photo": "./assets/viktorija-ism.jpg",
+    "facts": [
+      "14 metų skyrė šokių pasauliui",
+      "Mėgstamiausias vaizdas jai yra jūra ir uolos",
+      "Labai patinka patirti naujus dalykus ir emocijas"
+    ],
+    "tags": [
+      "ISM",
+      "Tarptautinis verslas",
+      "Komunikacija"
+    ],
+    "bio": "Viktorija dalijasi tarptautinio verslo ir komunikacijos studijų patirtimi bei padeda geriau suprasti ISM studentišką kasdienybę.",
+    "helpWith": [
+      "Klausimais apie tarptautinio verslo ir komunikacijos studijas",
+      "ISM kasdienybės ir studijų pasirinkimo aptarimu",
+      "Asmeninės patirties pasidalijimu prieš stojimą"
+    ]
   }
 ];
 window.ALUMNAS_SLOT_BLUEPRINTS = {
@@ -573,6 +825,634 @@ window.ALUMNAS_SLOT_BLUEPRINTS = {
       "dayOffset": 14,
       "times": [
         "21:00"
+      ]
+    }
+  ],
+  "rolandas-vdu": [
+    {
+      "dayOffset": 5,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 6,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 7,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 9,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 10,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 12,
+      "times": [
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 14,
+      "times": [
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 18,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 21,
+      "times": [
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 22,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 25,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00"
+      ]
+    },
+    {
+      "dayOffset": 26,
+      "times": [
+        "15:00"
+      ]
+    }
+  ],
+  "gabija-lsmu": [
+    {
+      "dayOffset": 1,
+      "times": [
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00"
+      ]
+    },
+    {
+      "dayOffset": 5,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 6,
+      "times": [
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00"
+      ]
+    },
+    {
+      "dayOffset": 8,
+      "times": [
+        "20:00",
+        "21:00"
+      ]
+    },
+    {
+      "dayOffset": 12,
+      "times": [
+        "12:00",
+        "13:00",
+        "20:00",
+        "21:00"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00"
+      ]
+    }
+  ],
+  "austeja-lsmu": [
+    {
+      "dayOffset": 2,
+      "times": [
+        "11:00",
+        "12:00",
+        "13:00"
+      ]
+    },
+    {
+      "dayOffset": 4,
+      "times": [
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 8,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00"
+      ]
+    },
+    {
+      "dayOffset": 9,
+      "times": [
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 11,
+      "times": [
+        "17:00"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "17:00",
+        "18:00"
+      ]
+    }
+  ],
+  "rasa-lmta": [
+    {
+      "dayOffset": 1,
+      "times": [
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 5,
+      "times": [
+        "19:00",
+        "19:45",
+        "20:30"
+      ]
+    },
+    {
+      "dayOffset": 6,
+      "times": [
+        "18:00",
+        "18:45"
+      ]
+    },
+    {
+      "dayOffset": 7,
+      "times": [
+        "19:00",
+        "19:45",
+        "20:30"
+      ]
+    },
+    {
+      "dayOffset": 8,
+      "times": [
+        "12:00",
+        "12:45",
+        "14:30",
+        "15:30",
+        "17:30"
+      ]
+    },
+    {
+      "dayOffset": 9,
+      "times": [
+        "10:00",
+        "10:45"
+      ]
+    }
+  ],
+  "nojus-vdu": [
+    {
+      "dayOffset": 4,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 5,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 6,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 7,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 8,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 11,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 12,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 14,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 15,
+      "times": [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00"
+      ]
+    }
+  ],
+  "odeta-viko": [
+    {
+      "dayOffset": 8,
+      "times": [
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 9,
+      "times": [
+        "15:00",
+        "16:00",
+        "17:00"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 14,
+      "times": [
+        "15:00",
+        "16:00"
+      ]
+    },
+    {
+      "dayOffset": 15,
+      "times": [
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 16,
+      "times": [
+        "15:00",
+        "16:00",
+        "17:00"
+      ]
+    }
+  ],
+  "leja-lsmu": [
+    {
+      "dayOffset": 4,
+      "times": [
+        "20:00"
+      ]
+    }
+  ],
+  "ieva-marija-ism": [
+    {
+      "dayOffset": 6,
+      "times": [
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 7,
+      "times": [
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 9,
+      "times": [
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 10,
+      "times": [
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 11,
+      "times": [
+        "12:00",
+        "13:00",
+        "14:00"
+      ]
+    },
+    {
+      "dayOffset": 12,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00"
+      ]
+    },
+    {
+      "dayOffset": 14,
+      "times": [
+        "13:00",
+        "14:00"
+      ]
+    },
+    {
+      "dayOffset": 16,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00"
+      ]
+    },
+    {
+      "dayOffset": 17,
+      "times": [
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00"
+      ]
+    }
+  ],
+  "viktorija-ism": [
+    {
+      "dayOffset": 0,
+      "times": [
+        "18:00"
+      ]
+    },
+    {
+      "dayOffset": 5,
+      "times": [
+        "17:00",
+        "18:00"
+      ]
+    },
+    {
+      "dayOffset": 6,
+      "times": [
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00"
+      ]
+    },
+    {
+      "dayOffset": 7,
+      "times": [
+        "18:00"
+      ]
+    },
+    {
+      "dayOffset": 12,
+      "times": [
+        "17:30",
+        "18:15"
+      ]
+    },
+    {
+      "dayOffset": 13,
+      "times": [
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00"
+      ]
+    },
+    {
+      "dayOffset": 14,
+      "times": [
+        "18:00"
       ]
     }
   ]
